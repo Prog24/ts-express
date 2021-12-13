d9f4f2ed4083105a131b8490b72911fc7c15f923
Prog24 <kenta@suzuki.dev>
2021-12-14 07:57:29 +0900

    wip

--- src/controller/Ranking.ts
+++ src/controller/Ranking.ts
@@ -1,55 +1,57 @@
 import express from 'express'
 // import { getRepository } from 'typeorm'
-import { getConnection } from 'typeorm'
+import { getConnection, getManager } from 'typeorm'
 import { Ranking as RankingModel } from 'src/entities/Ranking'
 import { RankingItem as RankingItemModel } from 'src/entities/RankingItem'
 
-const sample = (req: express.Request, res: express.Response) => {
-  const connection = getConnection()
+
+const createRanking = async (req: express.Request, res: express.Response) => {
   const title = req.body.title
   const description = req.body.description
   const items = req.body.items
-  const ranking_model = new RankingModel()
-  ranking_model.title = title
-  ranking_model.description = description
-
-  const ranking_items = items.map((item:any) => {
-    const ranking_item_model = new RankingItemModel()
-    ranking_item_model.title = item.title
-    ranking_item_model.description = item.description
-    return ranking_item_model
-  })
-  console.log(ranking_items)
-  ranking_model.ranking_items = ranking_items
-
-  connection.manager.save(ranking_model).then(success => {
-    res.send(success)
-  }).catch(err => {
-    res.send({ 'error': err.message })
-  })
-  // ranking_model.save().then(success => {
-  //   res.send(success)
-  //   // items.forEach((item:any) => {
-  //   //   const ranking_item_model = new RankingItemModel()
-  //   //   ranking_item_model.title = item.title
-  //   //   ranking_item_model.description = item.description
-  //   //   ranking_item_model.ranking = success
-  //   //   ranking_item_model.save().then(success => {
-  //   //     // 
-  //   //   }).catch(err => {
-  //   //     res.send({ 'error': err.message })
-  //   //   })
-  //   // })
-  //   // res.send({'success':'OK'})
-  // }).catch(err => {
-  //   res.send({'error': err.message})
-  // })
-
-
-  // res.send({
-  //   title: title,
-  //   description: description
-  // })
+  const connection = getConnection()
+  const queryRunner = connection.createQueryRunner()
+  await queryRunner.connect()
+  await queryRunner.startTransaction()
+  try {
+    const ranking_model = new RankingModel()
+    ranking_model.title = title
+    ranking_model.description = description
+    const save_ranking = await queryRunner.manager.save(ranking_model)
+    items.forEach(async (item: any) => {
+      const ranking_item_model = new RankingItemModel()
+      ranking_item_model.title = item.title
+      ranking_item_model.description = item.description
+      ranking_item_model.ranking = save_ranking
+      await queryRunner.manager.save(ranking_item_model)
+    })
+    await queryRunner.commitTransaction()
+  } catch (err) {
+    await queryRunner.rollbackTransaction()
+  } finally {
+    await queryRunner.release()
+  }
 }
 
-export { sample }
+// const createRanking = (req: express.Request, res: express.Response) => {
+//   const title = req.body.title
+//   const description = req.body.description
+//   const items = req.body.items
+//   const ranking_model = new RankingModel()
+//   ranking_model.title = title
+//   ranking_model.description = description
+//   ranking_model.save().then(success => {
+//     items.map((item: any) => {
+//       const ranking_item_model = new RankingItemModel()
+//       ranking_item_model.title = item.title
+//       ranking_item_model.description = item.description
+//       ranking_item_model.ranking = success
+//       ranking_item_model.save()
+//     })
+//     res.send({'success':'OK'})
+//   }).catch(err => {
+//     res.send({'error': err.message})
+//   })
+// }
+
+export { createRanking }
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { login, register } from './controller/User'
-import { sample } from './controller/Ranking'
+import { createRanking } from './controller/Ranking'
 
 const noAuthRouter = express.Router()
 const authRouter = express.Router()
@@ -8,6 +8,7 @@ const authRouter = express.Router()
 noAuthRouter.post('/login', login)
 noAuthRouter.post('/register', register)
 
-authRouter.post('/ranking', sample)
+// authRouter.post('/ranking', sample)
+authRouter.post('/ranking', createRanking)
 
 export { noAuthRouter, authRouter }
