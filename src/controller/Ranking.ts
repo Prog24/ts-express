f314de5ad47b4f0658f212e8175e0622fc3aa4af
Prog24 <kenta@suzuki.dev>
2021-12-15 16:21:35 +0900

    fix transaction

--- src/controller/Ranking.ts
+++ src/controller/Ranking.ts
@@ -1,6 +1,5 @@
 import express from 'express'
-// import { getRepository } from 'typeorm'
-import { getConnection, getManager } from 'typeorm'
+import { getManager } from 'typeorm'
 import { Ranking as RankingModel } from 'src/entities/Ranking'
 import { RankingItem as RankingItemModel } from 'src/entities/RankingItem'
 
@@ -9,31 +8,24 @@ const createRanking = async (req: express.Request, res: express.Response) => {
   const title = req.body.title
   const description = req.body.description
   const items = req.body.items
-  const connection = getConnection()
-  const queryRunner = connection.createQueryRunner()
-  await queryRunner.connect()
-  await queryRunner.startTransaction()
-  try {
+  await getManager().transaction(async transactionEntityManager => {
     const ranking_model = new RankingModel()
     ranking_model.title = title
     ranking_model.description = description
-    const save_ranking = await queryRunner.manager.save(ranking_model)
+    const save_ranking = await transactionEntityManager.save(ranking_model)
     const item_save_promise = items.map(async (item: any) => {
       const ranking_item_model = new RankingItemModel()
       ranking_item_model.title = item.title
       ranking_item_model.description = item.description
       ranking_item_model.ranking = save_ranking
-      return await queryRunner.manager.save(ranking_item_model)
+      return await transactionEntityManager.save(ranking_item_model)
     })
     await Promise.all(item_save_promise)
-    await queryRunner.commitTransaction()
-  } catch (err) {
-    await queryRunner.rollbackTransaction()
+  }).then(success => {
+    res.send({'success': 'ok'})
+  }).catch(err => {
     res.send({'error': err})
-  } finally {
-    await queryRunner.release()
-    res.send({'success':'ok'})
-  }
+  })
 }
 
 export { createRanking }
